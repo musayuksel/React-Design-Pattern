40250446b660e69839e11736e8d78ae0d6163c1c
Musa <79163385+musayuksel@users.noreply.github.com>
2022-07-05 12:20:07 +0100

    Add Router and update components

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import "./App.css";
 import SplitScreen from "./components/SplitScreen";
+import { Routes, Route } from "react-router-dom";
+import Nav from "./components/Nav";
 
 function App() {
   const LeftComponent = () => <h1 style={{ backgroundColor: "aqua" }}>LEFT</h1>;
   const RightComponent = () => <p style={{ backgroundColor: "pink" }}>Right</p>;
   return (
-    <SplitScreen
-      // left={LeftComponent}
-      // right={RightComponent}
-      leftWeight={1}
-      rightWeight={3}
-    >
-      {/* Pass as a children component */}
-      <LeftComponent />
-      <RightComponent />
-    </SplitScreen>
+    <>
+      <Nav />
+      <Routes>
+        <Route
+          path="/split-screen"
+          element={
+            <SplitScreen
+              // left={LeftComponent}
+              // right={RightComponent}
+              leftWeight={1}
+              rightWeight={3}
+            >
+              {/* Pass as a children component */}
+              <LeftComponent />
+              <RightComponent />
+            </SplitScreen>
+          }
+        />
+        <Route path="/list-items" element={<p>list item</p>} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Nav() {
+  return (
+    <ul>
+      <li>
+        <Link to={"split-screen"}>Split Screen</Link>
+      </li>
+      <li>
+        <Link to={"list-items"}>List Items</Link>
+      </li>
+    </ul>
+  );
+}
